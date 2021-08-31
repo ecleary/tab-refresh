41cec19c4436a4d7163d0bb84a728be937c3578b
Errol Cleary <errol.cleary@gmail.com>
2021-08-31 15:16:50 -0700

    Reformat with Prettier

--- client/src/components/ImageRow.jsx
+++ client/src/components/ImageRow.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { ImageThumbnail } from './ImageThumbnail.jsx';
-import styles from './styles/ImageRow.css';
+import React from "react";
+import { ImageThumbnail } from "./ImageThumbnail.jsx";
+import styles from "./styles/ImageRow.css";
 
 export const ImageRow = ({ imageData, onSetBackgroundImage }) => {
   const imageThumbnails = [];
   imageData.forEach((imageDetails) => {
-    imageThumbnails.push(<ImageThumbnail key={JSON.stringify(imageDetails)} imageData={imageDetails} onSetBackgroundImage={onSetBackgroundImage} />);
+    imageThumbnails.push(
+      <ImageThumbnail
+        key={JSON.stringify(imageDetails)}
+        imageData={imageDetails}
+        onSetBackgroundImage={onSetBackgroundImage}
+      />
+    );
   });
 
-  return (
-    <div className={styles.imageRowContainer}>
-      {imageThumbnails}
-    </div>
-  );
+  return <div className={styles.imageRowContainer}>{imageThumbnails}</div>;
 };
